a7f6af91fc74d7c9a6352aa3095e809bd5961e84
Caroline Jenuario <caaarolinejrs@gmail.com>
2023-08-03 15:35:50 -0300

    🚧 :construction: Add Identity Framework

---
+++ Entities/Entitities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Entitities
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Column("CPF")]
+        public string CPF { get; set; }
+    }
+}
